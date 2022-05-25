af6dfd2a24b038e06fe1a213d04fcabcfc958240
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-25 21:57:57 +0300

    Product of numbers in array

---
+++ Task005_product/Program.cs
@@ -0,0 +1,19 @@
+﻿// Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+Console.Write("Введите размер массива: ");
+int Number = int.Parse(Console.ReadLine() ?? "0");
+int[] array = new int[Number];
+for (int i = 0; i < array.Length; i++) 
+{
+    array[i] = new Random().Next(-10,11);
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+int indexStart = 0;
+int indexEnd = array.Length-1;
+while(indexStart<=indexEnd)
+{
+    int product = array[indexStart] * array[indexEnd];
+    Console.WriteLine($"Product of {indexStart} and {indexEnd} numbers of array is {product}.");
+    indexStart++;
+    indexEnd = indexEnd-1;
+}
