c89702074b4375d5c4e03c269ed66f1fe3981362
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-25 21:53:35 +0300

    Task004 The count of numbers from 10 to 99 in the array

---
+++ Task004_segment/Program.cs
@@ -0,0 +1,11 @@
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+int[] array = new int[123];
+for (int i = 0; i < array.Length; i++) 
+{
+    array[i] = new Random().Next(-100,100);
+    Console.Write($"{array[i]} ");
+}
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+    if(array[i]>=10 && array[i]<=99) count++;
+Console.Write($"The count of numbers from 10 to 99 in this array is {count}.");
